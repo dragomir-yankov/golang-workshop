7dc4e979b6abbba37f1d9f34cc88149b2f689211
Dragomir Yankov <dragomir.penev.yankov@gmail.com>
2018-10-06 11:47:22 +0300

    diagnostics

--- cmd/golang-workshop/main.go
+++ cmd/golang-workshop/main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/dragomir-yankov/golang-workshop/internal/diagnostics"
 )
 
 func main() {
 	log.Print("Hello, World")
 
-	err := http.ListenAndServe(":8080", nil)
+	router := mux.NewRouter()
+	router.HandleFunc("/", hello)
+
+	go func () {
+		err := http.ListenAndServe(":8080", nil)
+		if err != nil {
+			log.Fatal()
+		}
+	}()
+
+	diagnostics := diagnostics.NewDiagnostics() 
+	err := http.ListenAndServe(":8585", diagnostics)
 	if err != nil {
-		log.Fatal()
+		log.Fatal(err)
 	}
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, http.StatusText(http.StatusOK))
 }
